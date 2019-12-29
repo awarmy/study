f2316db50f3f06a5b92a0aa04bc38e0f7578f5de
warmy <35125421+awarmy@users.noreply.github.com>
2019-12-29 11:43:40 +0800

    vue-vuex

---
+++ vue/vue-vuex/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Couter></Couter>
+  </div>
+</template>
+
+<script>
+import Couter from "./components/Couter"
+
+export default {
+  name: 'app',
+  components: {
+    Couter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-vuex/src/components/Couter.vue
@@ -0,0 +1,44 @@
+<template>
+    <div><p>点击次数:{{count}}</p>
+        <button v-on:click="onincrement">++1</button><br /><br />
+        <button v-on:click="ondecrement">--1</button>
+    </div>
+</template>
+
+<script>
+// import { mapMutations } from "vuex"
+export default {
+    'name':'Couter',
+    data(){
+        return {
+        }
+    },
+    computed:{
+        count:function(){
+            return this.$store.state.count;
+        }
+    },
+    methods: {
+        //TODO: 方法一 展开方法
+        // ...mapMutations (['increment', 'decrement']),
+        onincrement:function(){
+            // this.increment();
+
+            //TODO: 方法二 同步方法
+            // this.$store.commit('increment')
+            
+            //TODO: 方法三 异步方法
+            this.$store.dispatch('increment');
+        },
+        ondecrement:function(){
+            // this.decrement();
+            // this.$store.commit('decrement');
+            this.$store.dispatch('decrement');
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
